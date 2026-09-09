c37eaef3325a933a66ac898ca3fe14301c14e8e1
RafidH777 <124240060@student.upnyk.ac.id>
2026-09-09 20:14:02 +0700

    fix home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,38 +3,106 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  // Daftar menu ditulis sebagai data supaya gampang ditambah/diubah
+  static const Color _primaryColor = Color(0xFF1A1A1A);
+
   static const List<_MenuItem> _menuItems = [
-    _MenuItem('Data Kelompok', Icons.group, '/kelompok'),
-    _MenuItem('Kalkulator', Icons.calculate, '/kalkulator'),
-    _MenuItem('Ganjil / Genap', Icons.filter_2, '/ganjil-genap'),
-    _MenuItem('Jumlah Total (Deret)', Icons.functions, '/deret'),
+    _MenuItem('Data Kelompok', Icons.groups_outlined, '/kelompok'),
+    _MenuItem('Kalkulator', Icons.calculate_outlined, '/kalkulator'),
+    _MenuItem('Ganjil / Genap', Icons.filter_2_outlined, '/ganjil-genap'),
+    _MenuItem('Jumlah Total', Icons.functions, '/deret'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Menu Utama'),
-        automaticallyImplyLeading: false, // tidak ada tombol back ke login
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 12),
+              const Text(
+                'Pilih Menu',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: _primaryColor,
+                ),
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: _menuItems.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    childAspectRatio: 1,
+                  ),
+                  itemBuilder: (context, index) {
+                    final item = _menuItems[index];
+                    return _MenuCard(
+                      title: item.title,
+                      icon: item.icon,
+                      onTap: () => Navigator.pushNamed(context, item.route),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(16),
-        itemCount: _menuItems.length,
-        separatorBuilder: (context, index) => const SizedBox(height: 12),
-        itemBuilder: (context, index) {
-          final item = _menuItems[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(item.icon, color: Colors.blue),
-              title: Text(item.title),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-              onTap: () {
-                Navigator.pushNamed(context, item.route);
-              },
-            ),
-          );
-        },
+    );
+  }
+}
+
+class _MenuCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const _MenuCard({
+    required this.title,
+    required this.icon,
+    required this.onTap,
+  });
+
+  static const Color _primaryColor = Color(0xFF1A1A1A);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(16),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(16),
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: _primaryColor, width: 1.2),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 40, color: _primaryColor),
+              const SizedBox(height: 12),
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  color: _primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screen/kelompok.dart
+++ lib/screen/kelompok.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class KelompokScreen extends StatelessWidget {
   const KelompokScreen({Key? key}) : super(key: key);
 
-  // TODO: ganti dengan nama anggota kelompok yang sebenarnya
+  static const Color _primaryColor = Color(0xFF1A1A1A);
+
+  // TODO: ganti dengan nama & NIM anggota kelompok yang sebenarnya
   static const List<Map<String, String>> _anggota = [
     {'nama': 'Nama Anggota 1', 'nim': 'NIM 1'},
     {'nama': 'Nama Anggota 2', 'nim': 'NIM 2'},
@@ -14,20 +16,111 @@ class KelompokScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Data Kelompok')),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: _anggota.length,
-        itemBuilder: (context, index) {
-          final anggota = _anggota[index];
-          return Card(
-            child: ListTile(
-              leading: CircleAvatar(child: Text('${index + 1}')),
-              title: Text(anggota['nama']!),
-              subtitle: Text(anggota['nim']!),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 12),
+              const Text(
+                'Data Kelompok',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: _primaryColor,
+                ),
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: _anggota.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    childAspectRatio: 0.9,
+                  ),
+                  itemBuilder: (context, index) {
+                    final anggota = _anggota[index];
+                    final nama = anggota['nama']!;
+                    final nim = anggota['nim']!;
+                    final inisial = nama.isNotEmpty ? nama[0].toUpperCase() : '?';
+
+                    return _AnggotaCard(
+                      inisial: inisial,
+                      nama: nama,
+                      nim: nim,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _AnggotaCard extends StatelessWidget {
+  final String inisial;
+  final String nama;
+  final String nim;
+
+  const _AnggotaCard({
+    required this.inisial,
+    required this.nama,
+    required this.nim,
+  });
+
+  static const Color _primaryColor = Color(0xFF1A1A1A);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: _primaryColor, width: 1.2),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 28,
+            backgroundColor: _primaryColor,
+            child: Text(
+              inisial,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          );
-        },
+          ),
+          const SizedBox(height: 12),
+          Text(
+            nama,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: _primaryColor,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            nim,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+        ],
       ),
     );
   }
